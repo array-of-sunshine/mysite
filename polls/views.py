b397b20e8279a62f0b3ffc97fc3394a62fe95cb1
Brian <riceb53@gmail.com>
2018-12-06 20:58:57 -0600

    make app, add route which returns json

---
+++ polls/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+
+
+def index(request):
+
+  some_data_to_dump = {
+     'some_var_1': 'foo',
+     'some_var_2': 'bar',
+  }
+
+  data = json.dumps(some_data_to_dump)
+
+  return HttpResponse(data, content_type='application/json')
+
